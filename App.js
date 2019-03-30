db7a0138b0db64afa19166a341fbcef4883c6393
Josh Hunter <thatsmegettinmail@yahoo.com>
2019-03-30 17:55:51 -0500

    add background image and styles

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import {
+         ImageBackground,
+         KeyboardAvoidingView,
+         Platform,
+         StyleSheet,
+         Text,
+         View,
+       } from 'react-native';
 
+import getImageForWeather from './utils/getImageForWeather';
 import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
@@ -10,12 +18,18 @@ export default class App extends React.Component {
       style={styles.container}
       behavior='padding'
       >
-        <Text style={[text.large, text.style]}>San Francisco</Text>
-        <Text style={[text.small, text.style]}>Lightly Cloudy</Text>
-        <Text style={[text.large, text.style]}>24 degrees</Text>
-
-        <SearchInput placeholder="Search any city" />
-
+        <ImageBackground
+          source={getImageForWeather('Clear')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+          >
+          <View style={styles.detailsContainer}>
+            <Text style={[text.large, text.style]}>San Francisco</Text>
+            <Text style={[text.small, text.style]}>Lightly Cloudy</Text>
+            <Text style={[text.large, text.style]}>24 degrees</Text>
+            <SearchInput placeholder="Search any city" />
+          </View>
+        </ImageBackground>
       </KeyboardAvoidingView>
     );
   }
@@ -24,10 +38,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#34495E',
   },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  }
 });
 
 const text = StyleSheet.create({
